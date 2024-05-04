61684aae4a3805e8b94fe9018b56a05f7f1bc4ab
Sebastian Carro <88806181+artifice911@users.noreply.github.com>
2024-05-04 18:50:31 -0300

    animacion

---
+++ main.js
@@ -0,0 +1,13 @@
+const pantallaContacto= document.getElementById("pantallaContacto").style.display="none";
+const pantallaRedes= document.getElementById("pantallaRedes").style.display="none";
+const pantallaMail= document.getElementById("pantallaMail").style.display="none";
+
+function mostrarContacto(){
+    const pantallaContacto= document.getElementById("pantallaContacto").style.display="flex";
+}
+function mostrarRedes(){
+    const pantallaContacto= document.getElementById("pantallaRedes").style.display="flex";
+}
+function mostrarMail(){
+    const pantallaContacto= document.getElementById("pantallaMail").style.display="flex";
+}
