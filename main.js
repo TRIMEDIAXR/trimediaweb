1e8555f94f0800032428e126cdff3ea4194725ec
Sebastian Carro <88806181+artifice911@users.noreply.github.com>
2024-05-05 06:11:41 -0300

    3dlisto

--- main.js
+++ main.js
@@ -5,24 +5,24 @@ const img1 = document.getElementById("animado1").style.display = "none";
 const img2 = document.getElementById("animado2").style.display = "none";
 const img3 = document.getElementById("animado3").style.display = "none";
 
-function mostrarPantalla(boton){
+function mostrarPantalla(boton) {
     const pantalla = document.getElementById("pantalla").style.display = "flex";
     switch (boton.id) {
         case "vr":
-            const textVR= document.getElementById("info").innerHTML="Diseño de videojuegos para Web, Realidad Virtual (VR), Realidad Aumentada (AR)."
-            const imgVR= document.getElementById("animado1").style.display="flex";
+            const textVR = document.getElementById("info").innerHTML = "Diseño de videojuegos para Web, Realidad Virtual (VR), Realidad Aumentada (AR)."
+            const imgVR = document.getElementById("animado1").style.display = "flex";
             const img2 = document.getElementById("animado2").style.display = "none";
             const img3 = document.getElementById("animado3").style.display = "none";
             break;
         case "hsi":
-            const textHSI= document.getElementById("info").innerHTML="Desarrollo de software y hardware para experiencias interactivas."
-            const imghsi= document.getElementById("animado1").style.display="none";
+            const textHSI = document.getElementById("info").innerHTML = "Desarrollo de software y hardware para experiencias interactivas."
+            const imghsi = document.getElementById("animado1").style.display = "none";
             const img = document.getElementById("animado2").style.display = "flex";
             const imgd = document.getElementById("animado3").style.display = "none";
-        break;
+            break;
         case "btl":
-            const textBTL= document.getElementById("info").innerHTML="Videojuegos para educación, capacitaciones, publicitarios y marcas (BTL)."
-            const imgVRw= document.getElementById("animado1").style.display="none";
+            const textBTL = document.getElementById("info").innerHTML = "Videojuegos para educación, capacitaciones, publicitarios y marcas (BTL)."
+            const imgVRw = document.getElementById("animado1").style.display = "none";
             const img2d = document.getElementById("animado2").style.display = "none";
             const img3s = document.getElementById("animado3").style.display = "flex";
             break;
@@ -30,16 +30,16 @@ function mostrarPantalla(boton){
     sonidoTap()
 }
 
-var type= new Typed('#txtAnimado',{
-    strings:[
-    '<i class="txtAnimado">EXPERIENCIAS INTERACTIVAS</i>',
-    '<i class="txtAnimado">VIDEOJUEGOS PERSONALIZADOS</i>',
-    '<i class="txtAnimado">VIDEOJUEGOS PARA EVENTOS</i>',
-    '<i class="txtAnimado">VIDEOJUEGOS PUBLICITARIOS</i>',
-    '<i class="txtAnimado">VIDEOJUEGOS EDUCATIVOS</i>',
-    '<i class="txtAnimado">REALIDAD VIRTUAL</i>',
-    '<i class="txtAnimado">REALIDAD AUMENTADA</i>'],
-    
+var type = new Typed('#txtAnimado', {
+    strings: [
+        '<i class="txtAnimado">EXPERIENCIAS INTERACTIVAS</i>',
+        '<i class="txtAnimado">VIDEOJUEGOS PERSONALIZADOS</i>',
+        '<i class="txtAnimado">VIDEOJUEGOS PARA EVENTOS</i>',
+        '<i class="txtAnimado">VIDEOJUEGOS PUBLICITARIOS</i>',
+        '<i class="txtAnimado">VIDEOJUEGOS EDUCATIVOS</i>',
+        '<i class="txtAnimado">REALIDAD VIRTUAL</i>',
+        '<i class="txtAnimado">REALIDAD AUMENTADA</i>'],
+
     typeSpeed: 70,
     startDelay: 250,
     backSpeed: 30,
@@ -55,7 +55,17 @@ var type= new Typed('#txtAnimado',{
     fadeOutDelay: 50
 })
 
-function sonidoTap(){
-    var tap= new Audio("audio/fxTap.mp3");
+function sonidoTap() {
+    var tap = new Audio("audio/fxTap.mp3");
     tap.play();
-  }
+}
+
+window.onload = function(){
+    
+    document.onclick = function(e){
+      if(e.target.id !== "pantalla" && e.target.id !== "vr" && e.target.id !== "hsi" && e.target.id !== "btl" && e.target.id !== "informacion"){
+        //element clicked wasn't the div; hide the div
+        const divToHide = document.getElementById("pantalla").style.display = "none";
+      }
+    };
+  }; 
