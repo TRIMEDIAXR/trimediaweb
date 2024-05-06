74759be170162cb1478ab21b40ab9a6773fc4425
Sebastian Carro <88806181+artifice911@users.noreply.github.com>
2024-05-05 21:58:09 -0300

    popup

--- main.js
+++ main.js
@@ -1,30 +1,68 @@
 const pantalla = document.getElementById("pantalla").style.display = "none";
+const form = document.getElementById("formulario").style.display = "none";
 const pantallaclass = document.getElementById("pantalla").style.flexDirection = "column";
 const pantallaAlign = document.getElementById("pantalla").style.alignContent = "center";
+const formclass = document.getElementById("formulario").style.flexDirection = "column";
+const formAlign = document.getElementById("formulario").style.alignContent = "center";
 const img1 = document.getElementById("animado1").style.display = "none";
 const img2 = document.getElementById("animado2").style.display = "none";
 const img3 = document.getElementById("animado3").style.display = "none";
 
+let auxiliar1= 0;
+let auxiliar2= 0;
+let auxiliar3= 0;
 function mostrarPantalla(boton) {
     const pantalla = document.getElementById("pantalla").style.display = "flex";
     switch (boton.id) {
-        case "vr":
-            const textVR = document.getElementById("info").innerHTML = "Diseño de videojuegos para Web, Realidad Virtual (VR), Realidad Aumentada (AR)."
-            const imgVR = document.getElementById("animado1").style.display = "flex";
-            const img2 = document.getElementById("animado2").style.display = "none";
-            const img3 = document.getElementById("animado3").style.display = "none";
-            break;
         case "hsi":
+            if(auxiliar1==0){
             const textHSI = document.getElementById("info").innerHTML = "Desarrollo de software y hardware para experiencias interactivas."
             const imghsi = document.getElementById("animado1").style.display = "none";
             const img = document.getElementById("animado2").style.display = "flex";
             const imgd = document.getElementById("animado3").style.display = "none";
+            const mostrarForm = document.getElementById("formulario").style.display = "none";
+            auxiliar1=1;
+            auxiliar2=0;
+            auxiliar3=0;
+            }
+            else{
+                auxiliar1=0;
+                const img = document.getElementById("pantalla").style.display = "none";
+            }
+
+            break;
+        case "vr":
+            if(auxiliar2==0){
+            const textVR = document.getElementById("info").innerHTML = "Diseño de videojuegos para Web, Realidad Virtual (VR), Realidad Aumentada (AR)."
+            const imgVR = document.getElementById("animado1").style.display = "flex";
+            const img2 = document.getElementById("animado2").style.display = "none";
+            const img3 = document.getElementById("animado3").style.display = "none";
+            const mostrarForm = document.getElementById("formulario").style.display = "none";
+            auxiliar2=1;
+            auxiliar1=0;
+            auxiliar3=0;
+            }
+            else{
+                auxiliar2=0;
+                const img = document.getElementById("pantalla").style.display = "none";
+            }
+            
             break;
         case "btl":
+            if(auxiliar3==0){
             const textBTL = document.getElementById("info").innerHTML = "Videojuegos para educación, capacitaciones, publicitarios y marcas (BTL)."
             const imgVRw = document.getElementById("animado1").style.display = "none";
             const img2d = document.getElementById("animado2").style.display = "none";
             const img3s = document.getElementById("animado3").style.display = "flex";
+            const mostrarForm = document.getElementById("formulario").style.display = "none";
+            auxiliar3=1;
+            auxiliar2=0;
+            auxiliar1=0;
+            }
+            else{
+                auxiliar3=0;
+                const img = document.getElementById("pantalla").style.display = "none";
+            }
             break;
     }
     sonidoTap()
@@ -60,12 +98,25 @@ function sonidoTap() {
     tap.play();
 }
 
-window.onload = function(){
-    
-    document.onclick = function(e){
-      if(e.target.id !== "pantalla" && e.target.id !== "vr" && e.target.id !== "hsi" && e.target.id !== "btl" && e.target.id !== "informacion"){
-        //element clicked wasn't the div; hide the div
-        const divToHide = document.getElementById("pantalla").style.display = "none";
-      }
+window.onload = function () {
+
+    document.onclick = function (e) {
+        if (e.target.id !== "vr" && e.target.id !== "hsi" && e.target.id !== "btl" && e.target.id !== "informacion") {
+            //element clicked wasn't the div; hide the div
+            const divToHide = document.getElementById("pantalla").style.display = "none";
+            const mostrarForm = document.getElementById("formulario").style.display = "none";
+            auxiliar1=0;
+            auxiliar2=0;
+            auxiliar3=0;
+        }
     };
-  }; 
+}; 
+
+function mostrarFormulario(){
+    const esconder = document.getElementById("pantalla").style.display = "none";
+    const mostrarForm = document.getElementById("formulario").style.display = "flex";
+    auxiliar1=0;
+    auxiliar2=0;
+    auxiliar3=0;
+
+}
